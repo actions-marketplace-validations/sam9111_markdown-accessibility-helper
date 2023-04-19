42e3c01733754b0febf317b6966384b353a7fcc1
sam9111 <samyuktha19095@cse.ssn.edu.in>
2023-04-19 16:18:05 +0530

    cleanup

--- suggest_alt_text.py
+++ suggest_alt_text.py
@@ -3,6 +3,7 @@ import re
 import requests
 import json
 
+# Post an image to the Azure Cognitive Services Computer Vision API to get a suggested alt text
 def suggest_alt_text(image_url,language='en'):
     subscription_key = os.environ['AZURE_SUBSCRIPTION_KEY']
     endpoint = os.environ['AZURE_ENDPOINT']+ 'vision/v3.2/describe'
@@ -14,6 +15,7 @@ def suggest_alt_text(image_url,language='en'):
     suggested_alt_text = response_data['description']['captions'][0]['text']
     return suggested_alt_text
 
+# Update the markdown file with the suggested alt text
 def update_markdown_file(file_path,language):
     with open(file_path, 'r') as f:
         content = f.read()
@@ -32,11 +34,13 @@ if __name__ == '__main__':
     repo_name = repo.split('/')[1]
     language = os.environ['ALT_LANGUAGE']
     clone_url = f'https://github.com/{repo}.git'
+
     if os.environ['CLONE_URL']:
         clone_url = os.environ['CLONE_URL']
     branch = 'main'
     if os.environ['BRANCH']:
         branch = os.environ['BRANCH']
+
     os.system(f"git clone --depth=1 --branch={branch} {clone_url} repo")
     os.chdir('repo')
 
@@ -44,6 +48,8 @@ if __name__ == '__main__':
         if filename.endswith('.md'):
             update_markdown_file(filename,language)
             os.system(f"git add {filename}")
+
+    # Commit and push changes
     github_username = os.environ['GITHUB_ACTOR']
     os.system(f'git config --global user.email "{github_username}@users.noreply.github.com"')
     os.system(f'git config --global user.name "{github_username}"')
